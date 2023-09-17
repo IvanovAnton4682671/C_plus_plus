0bb06de9f20b8e850dba6380fc7c0369628ecaa5
Ivanov Anton <112862863+IvanovAnton4682671@users.noreply.github.com>
2023-09-17 13:51:46 +0300

    Add files via upload

---
+++ ТестоваяПрограммаКлиентСервер/Клиент/Клиент.cpp
@@ -0,0 +1,51 @@
+﻿//Программа-клиент
+
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+
+using namespace std;
+
+struct Person
+{
+	char name[25];
+	int height;
+	int weight;
+}A;
+
+void main ()
+{
+	setlocale (LC_ALL, "rus");
+	const char* nameR = "REQUEST.bin";
+	const char* nameA = "ANSWER.bin";
+	ofstream f_REQ;
+	ifstream f_ANS;
+	long pred_size;
+	int answer;
+	cout << "Клиент работает нормально." << endl;
+	while (true)
+	{
+		cout << "Введите данные: фамилия, рост, вес:" << endl;
+		cin >> A.name >> A.height >> A.weight;
+		f_REQ.open(nameR, ios::app | ios::binary);
+		f_REQ.write((char*)&A, sizeof(A));
+		f_REQ.close();
+		f_ANS.open(nameA, ios::binary);
+		f_ANS.seekg(0, ios::end);
+		pred_size = f_ANS.tellg();
+		while (pred_size >= f_ANS.tellg())
+		{
+			Sleep(100);
+			f_ANS.seekg(0, ios::end);
+		}
+		f_ANS.seekg(pred_size, ios::beg);
+		f_ANS.read((char*)&answer, sizeof(answer));
+		f_ANS.close();
+		switch (answer)
+		{
+		case 0: {cout << "У вас нормальный вес." << endl; break; }
+		case 1: {cout << "У вас недостаток веса." << endl; break; }
+		case 2: {cout << "У вас избыток веса." << endl; break; }
+		}
+	}
+}
---
+++ ТестоваяПрограммаКлиентСервер/ТестоваяПрограммаКлиентСервер/ТестоваяПрограммаКлиентСервер.cpp
@@ -0,0 +1,55 @@
+﻿//Программа-сервер
+
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+
+using namespace std;
+
+struct Person
+{
+	char name[25];
+	int height;
+	int weight;
+}B;
+
+int answer;
+long size_pred;
+
+int main ()
+{
+	setlocale (LC_ALL, "rus");
+	ifstream fR;
+	ofstream fA;
+	const char* nameR = "REQUEST.bin";
+	const char* nameA = "ANSWER.bin";
+	cout << "Сервер работает. Можете работать с клиентом." << endl;
+	fR.open(nameR, ios::binary);
+	fR.seekg(0, ios::end);
+	size_pred = fR.tellg();
+	fR.close();
+	while (true)
+	{
+		fR.open(nameR, ios::binary);
+		fR.seekg(0, ios::end);
+		while (size_pred >= fR.tellg())
+		{
+			Sleep(100);
+			fR.seekg(0, ios::end);
+		}
+		fR.seekg(size_pred, ios::beg);
+		fR.read((char*)&B, sizeof(B));
+		size_pred = fR.tellg();
+		fR.close();
+		double IMT = B.weight / (0.01 * B.height) / (0.01 * B.height);
+		if (18.5 <= IMT && IMT < 25)
+			answer = 0;
+		if (18.5 > IMT)
+			answer = 1;
+		if (IMT >= 25)
+			answer = 2;
+		fA.open(nameA, ios::binary | ios::app);
+		fA.write((char*)&answer, sizeof(answer));
+		fA.close();
+	}
+}
