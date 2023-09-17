20429fc3c80a03e71e26b3a5ed11094fbb56006d
Ivanov Anton <112862863+IvanovAnton4682671@users.noreply.github.com>
2023-09-17 13:54:54 +0300

    Add files via upload

---
+++ ТретьяРабота/ПерваяРаботаКлиентСерверUDP/Клиент/Клиент.cpp
@@ -0,0 +1,98 @@
+﻿//client
+#include <iostream>
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+// подавление предупреждений библиотеки winsock2
+#include <winsock2.h>
+#include <string>
+#include <windows.h>
+#pragma comment (lib, "ws2_32.lib")
+#pragma warning(disable: 4996) // подавление предупреждения 4996
+using namespace std;
+#define PORT 999
+#define SERVERADDR "127.0.0.1"//порт сервера
+
+struct Person {
+	int height;//рост
+	int weight;//вес
+}A;
+
+bool findSymbol(string s) {
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] >= 'A' && s[i] <= 'Z' || s[i] >= 'a' && s[i] <= 'z') {
+			return true;
+		}
+	}
+	return false;
+}
+
+int main() {
+	char buff[10 * 1024];
+	if (WSAStartup(0x0202, (WSADATA*)&buff)) {
+		cout << "Error WSAStartup \n" << WSAGetLastError(); // Ошибка!
+		return -1;
+	}
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	SOCKET ms = socket(AF_INET, SOCK_DGRAM, 0);//сокет клиента
+	if (ms == INVALID_SOCKET) {
+		cout << "SOCKET() ERROR: " << WSAGetLastError() << "\n";
+		WSACleanup();
+		return -1;
+	}
+	hostent* hst;
+	sockaddr_in Daddr;
+	Daddr.sin_family = AF_INET;
+	Daddr.sin_port = htons(PORT);
+	if (inet_addr(SERVERADDR))
+		Daddr.sin_addr.s_addr = inet_addr(SERVERADDR);
+	else if (hst = gethostbyname(SERVERADDR))
+		Daddr.sin_addr.s_addr = ((unsigned long**)hst->h_addr_list)[0][0];
+	else {
+		cout << "Unknown Host: " << WSAGetLastError() << "\n";
+		closesocket(ms);
+		WSACleanup();
+		return -1;
+	}
+
+	while (1) {
+		string s;
+		cout << "Введите любой текст чтобы начать или bye чтобы выйти" << endl;
+		cin >> s;
+		if (s == "bye")
+			break;
+		sendto(ms, (char*)&s[0], s.size(), 0, (sockaddr*)&Daddr, sizeof(Daddr));
+
+		sockaddr_in SRaddr;
+		int SRaddr_size = sizeof(SRaddr);
+		cout << "Введите рост: ";
+		cin >> s;
+		if (findSymbol(s)) {
+			cout << "ошибка ввода.\nВводить нужно только числа!" << endl;
+			continue;
+		}
+		A.height = stoi(s);
+		cout << "Введите вес: ";
+		cin >> s;
+		if (findSymbol(s)) {
+			cout << "ошибка ввода.\nВводить нужно только числа!" << endl;
+			continue;
+		}
+		A.weight = stoi(s);
+
+		sendto(ms, (char*)&A, sizeof(A), 0, (sockaddr*)&Daddr, sizeof(Daddr));//отправил
+		int n = recvfrom(ms, buff, sizeof(buff), 0, (sockaddr*)&SRaddr, &SRaddr_size);//прочитал
+		if (n == SOCKET_ERROR) {
+			cout << "recform error: " << WSAGetLastError() << "\n";
+			closesocket(ms);
+			WSACleanup();
+			return -1;
+		}
+		//cout << A.imt << endl;
+		buff[n] = '\0';
+		cout << buff << endl;
+	}
+	closesocket(ms);
+	WSACleanup();
+	system("pause");
+	return 0;
+}
---
+++ ТретьяРабота/ПерваяРаботаКлиентСерверUDP/Сервер/Сервер.cpp
@@ -0,0 +1,105 @@
+﻿//server
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <iostream>
+#include <winsock2.h>
+#include <windows.h>
+#include <string>
+#pragma comment (lib, "ws2_32.lib")
+#pragma warning(disable: 4996)
+using namespace std;
+#define PORT 999
+
+struct Person {
+	int height;//рост
+	int weight;//вес
+}B;
+
+double IMT(double h, double w) {
+	return (w / ((h / 100) * (h / 100)));
+}
+
+string F(double imt) {
+	string s;
+	if (imt < 16) {
+		s = "Выраженный дефицит массы тела";
+		return s;
+	}
+	else if (imt < 18.5) {
+		s = "Недостаточная (дефицит) масса тела";
+		return s;
+	}
+	else if (imt < 25) {
+		s = "Норма";
+		return s;
+	}
+	else if (imt < 30) {
+		s = "Избыточная масса тела (предожирение)";
+		return s;
+	}
+	else if (imt < 35) {
+		s = "Ожирение первой степени";
+		return s;
+	}
+	else if (imt < 40) {
+		s = "Ожирение второй степени";
+		return s;
+	}
+	else if (imt > 40) {
+		s = "Ожирение третьей степени (морбидное)";
+		return s;
+	}
+	else {
+		return "xp";
+	}
+}
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	char buff[1024];
+	if (WSAStartup(0x0202, (WSADATA*)&buff[0])) //инициализация в библиотеке.1)версия в библиотеке 2)структура для записи
+	{
+		cout << "Error WSAStartup \n" << WSAGetLastError(); // Ошибка!
+		return -1;
+	}
+	SOCKET Lsock = socket(AF_INET, SOCK_DGRAM, 0);
+	if (Lsock == INVALID_SOCKET)
+	{
+		cout << "SOCKET() ERROR: " << WSAGetLastError() << "\n";
+		WSACleanup();
+		return -1;
+	}
+	sockaddr_in Laddr; //структуры
+	Lsock = socket(AF_INET, SOCK_DGRAM, 0);//создание слушащего сокета сервера.1)Домен 2)Потоковый режим 3)TCP
+	Laddr.sin_family = AF_INET;
+	Laddr.sin_addr.s_addr = INADDR_ANY;
+	Laddr.sin_port = htons(PORT);
+	if (bind(Lsock, (sockaddr*)&Laddr, sizeof(Laddr)))
+	{
+		cout << "BIND ERROR: " << WSAGetLastError();
+		closesocket(Lsock);
+		WSACleanup();
+		return -1;
+	}
+	while (1) {
+
+		sockaddr_in Caddr;
+		int Caddr_size = sizeof(Caddr);
+		int bsize = recvfrom(Lsock, &buff[0], sizeof(buff) - 1, 0, (sockaddr*)&Caddr, &Caddr_size);//получил
+		if (bsize == SOCKET_ERROR)  cout << "recform error: " << WSAGetLastError();
+		hostent* hst;
+		hst = gethostbyaddr((char*)&Caddr.sin_addr, 4, AF_INET);
+		cout << "NEW DATAGRAM!\n" << ((hst) ? hst->h_name : "Unkown host") << "\n" << inet_ntoa(Caddr.sin_addr) << "\n" << ntohs(Caddr.sin_port) << '\n';
+		int bsize2 = recvfrom(Lsock, (char*)&B, sizeof(buff) - 1, 0, (sockaddr*)&Caddr, &Caddr_size);
+		if (bsize2 == SOCKET_ERROR)  cout << "recform2 error: " << WSAGetLastError();
+		string s;
+		double imt1 = IMT(B.height, B.weight);
+		s = to_string(imt1);
+		//sendto(Lsock, (char*)&B, sizeof(B), 0, (sockaddr*)&Caddr, sizeof(Caddr));
+		s += "\n";
+		s += F(imt1);
+		sendto(Lsock, &s[0], s.size(), 0, (sockaddr*)&Caddr, sizeof(Caddr));
+	}
+	system("pause");
+	return 0;
+}
