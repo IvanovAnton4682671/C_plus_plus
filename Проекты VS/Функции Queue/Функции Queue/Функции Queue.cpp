31e23eb0677bf6134a39ceb1edbfc0c7adafdb84
Ivanov Anton <112862863+IvanovAnton4682671@users.noreply.github.com>
2023-09-17 14:11:12 +0300

    Add files via upload

---
+++ Проекты VS/Функции Queue/Функции Queue/Функции Queue.cpp
@@ -0,0 +1,54 @@
+﻿//Для работы очереди нужны 4 операции: инициализировать очередь, добавить элемент в конец очереди, извлечь элемент из начала очереди, булевская операция "очередь не пуста" (проверка на пустоту).
+
+struct queue
+{
+	int *q;
+	int n;
+	int k;
+};
+
+void init(queue &qu)
+{
+	qu.q = new int[100];
+	qu.n = 0;
+	qu.k = -1;
+}
+
+void push(queue &qu, int x)
+{
+	if (qu.k < 99)
+	{
+		qu.k++;
+		qu.q[qu.k] = x;
+	}
+	else
+		if (qu.n == 0)
+			cout << "Очередь переполнена." << endl;
+		else
+		{
+			for (int i = qu.n; i <= qu.k; i++)
+				qu.q[i - qu.n] = qu.q[i];
+			qu.k -= qu.n;
+			qu.n = 0;
+			qu.k++;
+			qu.q[qu.k] = x;
+		}
+}
+
+int pop(queue &qu)
+{
+	int x = -9999;
+	if (qu.k >= qu.n)
+	{
+		x = qu.q[qu.n];
+		qu.n++;
+	}
+	else
+		cout << "Очередь пуста." << endl;
+	return x;
+}
+
+bool notempty(queue &qu)
+{
+	return qu.k >= qu.n;
+}
---
+++ Проекты VS/Функции Stack`а/Функции Stack`а/Функции Stack`а.cpp
@@ -0,0 +1,42 @@
+﻿//Для работы стэка нужны 4 операции: инициализировать стэк, добавить элемент в стэк, извлечь элемент из стэка, булевская операция "стэк не пуст" (проверка на пустоту).
+
+struct stack
+{
+	int *s;
+	int top;
+};
+
+void init(stack &st)
+{
+	st.s = new int[100];
+	st.top = -1;
+}
+
+void push(stack &st, int x)
+{
+	if (st.top < 99)
+	{
+		st.top++;
+		st.s[st.top] = x;
+	}
+	else
+		cout << "Стэк переполнен." << endl;
+}
+
+int pop(stack &st)
+{
+	int x = -9999;
+	if (st.top > -1)
+	{
+		x = st.s[st.top];
+		st.top--;
+	}
+	else
+		cout << "Стэк пуст." << endl;
+	return x;
+}
+
+bool notempty(stack &st)
+{
+	return st.top > -1;
+}
