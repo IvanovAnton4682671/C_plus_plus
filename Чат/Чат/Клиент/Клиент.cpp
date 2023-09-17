5c8e8e0212e83170228ba4c0343ffacee50d499e
Ivanov Anton <112862863+IvanovAnton4682671@users.noreply.github.com>
2023-09-17 13:53:39 +0300

    Add files via upload

---
+++ Чат/Чат/Клиент/Клиент.cpp
@@ -0,0 +1,121 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
+#include <windows.h>
+#include <string>
+#include <iostream>
+#pragma comment(lib, "ws2_32.lib")
+#pragma warning(disable: 4996)
+using namespace std;
+
+SOCKET client;
+enum Packet {
+    Pack,
+    Test
+};
+
+DWORD WINAPI ClientThread(LPVOID sock)
+{ //функция потока для принятия сообщений
+    SOCKET connection = ((SOCKET*)sock)[0];
+    Packet packet;
+
+    while (1)
+    { //получения пакетов от сервера
+        recv(connection, (char*)&packet, sizeof(Packet), NULL);
+        Sleep(1000);
+        if (packet == Pack)
+        {
+            int messageSize;
+            recv(connection, (char*)&messageSize, sizeof(int), NULL); //размер пакета
+            char* message = new char[messageSize + 1]; //резервирование буффера       
+            message[messageSize] = '\0';
+
+            recv(connection, message, messageSize, NULL); //прием данных пакета
+            cout << message << endl;
+            delete[] message;
+        }
+        else
+        {
+            if (packet == Test) //прием сигнального пакета от сервера
+                cout << "Это тестовое сообщение.\n";
+            else
+            {
+                cout << "Unrecognized packet: " << packet << endl;
+                break;
+            }
+        }
+    }
+    closesocket(client);
+    return 0;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    WSAData wsaData;
+    WORD Ver = MAKEWORD(2, 1);
+    if (WSAStartup(Ver, &wsaData) != 0)
+    {
+        cout << "Error" << endl;
+        return 1;
+    }
+
+    //определение параметров сервера
+    SOCKADDR_IN addr;
+    int sizeofaddr = sizeof(addr);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_port = htons(666);
+    addr.sin_family = AF_INET;
+    client = socket(AF_INET, SOCK_STREAM, NULL);
+
+    if (connect(client, (SOCKADDR*)&addr, sizeof(addr)) != 0) //соединение с сервером
+    {
+        cout << "Error: failed connect to server.\n";
+        return 1;
+    }
+    cout << "Вы подключились.\n";
+
+    while (true)
+    {
+        cout << "Никнейм: "; //создание имени
+        string username;
+        cin >> username;
+        send(client, (char*)&username[0], 1024, NULL);
+
+        char* response = new char[1024];
+        recv(client, response, 1024, NULL);
+        string stringResponse = response;
+        if (stringResponse == "OK")
+            break;
+        else
+            cout << response << "\n";
+    }
+
+    CreateThread(NULL, NULL, ClientThread, &client, NULL, NULL); //создаем дочерний поток для приема сообщений от сервера
+
+    string message;
+    while (true)
+    { //отправка данных серверу в основной поток
+        getline(cin, message);
+        int messageSize = message.size();
+        Packet packettype = Pack; //передача информационного пакета
+        send(client, (char*)&packettype, sizeof(Packet), NULL);
+        send(client, (char*)&messageSize, sizeof(int), NULL);
+        send(client, (char*)&message[0], messageSize, NULL);
+        if (message == "/exit")
+        {
+            cout << "До свидания!\n";
+            break;
+        }
+        if (message == "/private")
+        {
+            cout << "Вы вошли в приватный канал. Введите никнейм друга: (для выхода введите '/private-off') ";
+            string friendUsername;
+            cin >> friendUsername;
+            send(client, (char*)&friendUsername[0], 1024, NULL);
+        }
+        Sleep(1000);
+    }
+    closesocket(client);
+    return 0;
+}
---
+++ Чат/Чат/Сервер/Сервер.cpp
@@ -0,0 +1,238 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
+#include <windows.h>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#pragma comment(lib, "ws2_32.lib")
+#pragma warning(disable: 4996)
+using namespace std;
+
+SOCKET clients[10]; //массив сокетов клиентов
+char* username;
+
+vector<char*> usernames(10); //имена всех пользователей
+
+int clientsCounter = 0; //количество клиентов
+enum Packet { Pack, Test }; //допустимые типы пакетов
+
+
+int findIndexByUsername(char* username1)
+{
+    string str1, str2;
+    str1 = username1;
+    for (int i = 0; i < 10; i++)
+    {
+        if (usernames[i] != NULL)
+        {
+            str2 = usernames[i];
+            if (str2 == str1)
+                return i;
+        }
+    }
+    return -1;
+}
+
+
+
+DWORD WINAPI ServerThread(LPVOID number)
+{ //обработка потока клиента
+    Packet packet;
+    bool isPrivate = false;
+    char* friendUsername = NULL;
+
+    int index = ((int*)number)[0]; //индекс обрабатываемого сокета
+    cout << "Номер сокета пользователя: " << index << endl;
+    cout << "\n";
+
+    SOCKET client = clients[index];
+
+    //общение с клиентом
+    while (true)
+    {
+        recv(client, (char*)&packet, sizeof(Packet), NULL); //получение информации от клиента
+
+        //определение типа полученного пакета
+        if (packet == Pack)
+        {
+            int messageSize;
+            recv(client, (char*)&messageSize, sizeof(int), NULL); //определение размера пакета
+
+            char* message = new char[messageSize + 1 + sizeof(username)]; //резервируем буффер нужного размера
+            message[messageSize] = 0;
+            recv(client, message, messageSize, NULL);  //получение пакета
+
+            string responseMessage = message;
+
+
+
+            if (responseMessage == "/exit")
+            {
+                cout << "Пользователь " << usernames[index] << " отключился.\n";
+                break;
+            }
+
+            if (responseMessage == "/private")
+            {
+                friendUsername = new char[1024];
+                recv(client, friendUsername, 1024, NULL);
+                cout << "Никнейм друга: " << friendUsername << "\n";
+
+                isPrivate = true;
+            }
+
+            if (responseMessage == "/private-off")
+            {
+                isPrivate = false;
+            }
+
+            if (responseMessage == "" || responseMessage.empty())
+                continue;
+
+
+
+            string response = usernames[index];
+            response += ": ";
+            response += message;
+            cout << response << "\n";
+
+            messageSize = response.size();
+
+            //передаем сообщение другим участникам чата
+            if (!isPrivate)
+            { //если не приватный чат, то передаем всем участникам
+                for (int i = 0; i < 10; i++)
+                {
+                    if (i == index || clients[i] == NULL)
+                        continue;
+
+                    Packet messageType = Pack;
+                    send(clients[i], (char*)&messageType, sizeof(Packet), NULL); //передача типа
+                    send(clients[i], (char*)&messageSize, sizeof(int), NULL); //передача размера
+                    send(clients[i], (char*)&response[0], messageSize, NULL); //передача самого сообщения
+                }
+                delete[] message;
+            }
+            else
+            { //если приватный, то находим индекс клиента по имени и передаем только ему
+                Packet messageType = Pack;
+                int i = findIndexByUsername(friendUsername);
+                cout << "Номер друга: " << i << "\n";
+                send(clients[i], (char*)&messageType, sizeof(Packet), NULL); //передача типа
+                send(clients[i], (char*)&messageSize, sizeof(int), NULL); //передача размера
+                send(clients[i], (char*)&response[0], messageSize, NULL); //передача самого сообщения 
+            }
+        }
+        else //получен неопознанный пакет
+        {
+            cout << "Unrecognized packet: " << packet << endl; break;
+        }
+    }
+    closesocket(client);
+    usernames[index] = NULL; //клиент ушел, имя освобождено
+    clientsCounter--;
+    cout << "Количество пользователей: " << clientsCounter << "\n";
+    return 0;
+}
+
+bool checkUsername(char* username, int index)
+{ //проверка: занято ли имя
+    string str1, str2;
+    str1 = username;
+    for (char* name : usernames)
+    {
+        if (name != NULL)
+        {
+            str2 = name;
+            if (str1 == str2)
+                return false;
+        }
+    }
+    usernames[index] = username;
+    return true;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    WSAData wsaData; //инициализация Winsock
+    WORD DllVersion = MAKEWORD(2, 1);
+    if (WSAStartup(DllVersion, &wsaData) != 0)
+    {
+        cout << "Error" << endl; exit(1);
+    }
+
+    //сохраняем в слушающем сокете информацию о сервере
+    SOCKADDR_IN addr;
+    int sizeofaddr = sizeof(addr);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_port = htons(666);
+    addr.sin_family = AF_INET;
+
+
+    SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, NULL);
+
+    bind(listenSocket, (SOCKADDR*)&addr, sizeof(addr));
+    listen(listenSocket, 10); //прослушивание и организация очереди из 10 клиентов
+
+    SOCKET newConnection;
+
+    for (int i = 0; i < 10; i++)
+    {
+        newConnection = accept(listenSocket, (SOCKADDR*)&addr, &sizeofaddr); //извлекаем запрос из очереди
+        if (newConnection == 0)
+        {
+            cout << "Error #2\n";
+        }
+        else
+        {
+            cout << "\nПодключился новый пользователь.\n";
+            string message;
+            int messageSize;
+            username = new char[1024];
+
+            //получение имени и обработка
+            while (true)
+            {
+                recv(newConnection, username, 1024, NULL);
+                cout << "Никнейм: " << username << "\n";
+
+                if (!checkUsername(username, i))
+                {
+                    message = "Этот никнейм занят, попробуйте другой.";
+                    messageSize = message.size();
+                    send(newConnection, (char*)&message[0], 1024, NULL);
+                }
+                else
+                {
+                    message = "OK";
+                    messageSize = message.size();
+                    send(newConnection, (char*)&message[0], 1024, NULL);
+
+                    Sleep(1000);
+                    break;
+                }
+            }
+            Packet messageType = Pack;
+            send(newConnection, (char*)&messageType, sizeof(Packet), NULL); //передаем тип
+            send(newConnection, (char*)&messageSize, sizeof(int), NULL); //размер сообщения
+            send(newConnection, (char*)&message[0], messageSize, NULL); //само сообщение
+
+            clients[i] = newConnection; //сохраняем принятого клиента в массив участников чата
+            clientsCounter++;
+            cout << "Номер пользователя: " << clientsCounter << endl;
+
+            CreateThread(NULL, NULL, ServerThread, &i, NULL, NULL); //создаем новый поток для обслуживания клиента
+            Sleep(1000);
+
+
+            Packet testpacket = Test; //передача клиенту сигнального сообщения
+            cout << "Это тестовое сообщение.\n";
+            send(newConnection, (char*)&testpacket, sizeof(Packet), NULL);
+
+        }
+    }
+    return 0;
+}
