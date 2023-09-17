9b812ee3b8a5bb2847d6681c573a64e0acea9b06
Ivanov Anton <112862863+IvanovAnton4682671@users.noreply.github.com>
2023-09-17 13:49:02 +0300

    Add files via upload

---
+++ ВтораяРабота/ПерваяПрактикаКлиентСерверTCP/СерверКлиентTCP/КлиентСерверTCP/КлиентСерверTCP.cpp
@@ -0,0 +1,98 @@
+﻿//client
+
+#include <iostream>
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+// подавление предупреждений библиотеки winsock2
+#include <winsock2.h>
+#include <string>
+#include <windows.h>
+
+#include <string>
+#pragma comment (lib, "Ws2_32.lib")
+#pragma warning(disable: 4996) // подавление предупреждения 4996
+using namespace std;
+
+struct Person
+{
+	int height, weight;//рост, вес
+	string IMT;
+	//string text;
+}A;
+
+bool findSymbol(string s) {
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] >= 'A' && s[i] <= 'Z' || s[i] >= 'a' && s[i] <= 'z') {
+			return true;
+		}
+	}
+	return false;
+}
+
+#define SRV_HOST "localhost"
+#define SRV_PORT 1234
+#define CLNT_PORT 1235
+#define BUF_SIZE 128
+char TXT_ANSW[] = "I am your student\n";
+int main() {
+	setlocale(LC_ALL, "rus");
+	char buff[1024];
+	if (WSAStartup(0x0202, (WSADATA*)&buff[0])) {
+		cout << "Error WSAStartup \n" << WSAGetLastError(); // Ошибка!
+		return -1;
+	}
+	SOCKET s;
+	int from_len; char buf[BUF_SIZE] = { 0 }; hostent* hp;
+	sockaddr_in clnt_sin, srv_sin;
+	s = socket(AF_INET, SOCK_STREAM, 0);
+	clnt_sin.sin_family = AF_INET;
+	clnt_sin.sin_addr.s_addr = 0;
+	clnt_sin.sin_port = htons(CLNT_PORT);
+	bind(s, (sockaddr*)&clnt_sin, sizeof(clnt_sin));
+	hp = gethostbyname(SRV_HOST);
+	srv_sin.sin_port = htons(SRV_PORT);
+	srv_sin.sin_family = AF_INET;
+	((unsigned long*)&srv_sin.sin_addr)[0] =
+		((unsigned long**)hp->h_addr_list)[0][0];
+	connect(s, (sockaddr*)&srv_sin, sizeof(srv_sin));
+	string mst = "";
+	do {
+		cout << "Введите рост: ";
+		cin >> mst;
+		if (findSymbol(mst)) {
+			if (mst == "bye") {
+				A.height = -1;
+				send(s, (char*)&A, sizeof(A), 0);
+				break;
+			}
+			else
+				cout << "ошибка ввода.\nВводить нужно только числа" << endl;
+			continue;
+		}
+		else {
+			A.height = stoi(mst);
+		}
+		cout << "Введите вес: ";
+		cin >> mst;
+		if (findSymbol(mst)) {
+			if (mst == "bye")
+				break;
+			else
+				cout << "ошибка ввода.\nВводить нужно только числа" << endl;
+			continue;
+		}
+		else {
+			A.weight = stoi(mst);
+		}
+		A.IMT = "0";
+		//A.text = "0";
+		send(s, (char*)&A, sizeof(A), 0);//отправил структуру
+		from_len = recv(s, (char*)&A, sizeof(A), 0);
+		buf[from_len] = 0;
+
+		cout << "Ваш ИМТ = " << A.IMT << endl;
+		//cout << A.text << endl;
+	} while (mst != "bye");
+	cout << "exit to infinity" << endl;
+	cin.get(); closesocket(s);
+	return 0;
+}
---
+++ ВтораяРабота/ПерваяПрактикаКлиентСерверTCP/СерверКлиентTCP/СерверКлиентTCP/СерверКлиентTCP.cpp
@@ -0,0 +1,76 @@
+﻿//serv
+
+#include <iostream>
+#include <winsock2.h>
+#include <windows.h>
+#include <string>
+#pragma comment (lib, "Ws2_32.lib")
+using namespace std;
+
+struct Person
+{
+	int height, weight;//рост, вес
+	string IMT;
+	//string text;
+}B;
+
+#define SRV_PORT 1234
+#define BUF_SIZE 128
+const string QUEST = "Who are you?\n";
+
+double IMG(double h, double w) {
+	return (w / ((h / 100) * (h / 100)));
+}
+
+string F(double img) {
+	string s;
+	if (img < 16) {
+		s = "Выраженный дефицит массы тела";
+		return s;
+	}
+	else {
+		return "xp";
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	char buff[1024];
+	if (WSAStartup(0x0202, (WSADATA*)&buff[0]))
+	{
+		cout << "Error WSAStartup \n" << WSAGetLastError(); // Ошибка!
+		return -1;
+	}
+	SOCKET s, s_new;
+	int from_len;
+	char buf[BUF_SIZE] = { 0 };
+	sockaddr_in sin, from_sin;
+	s = socket(AF_INET, SOCK_STREAM, 0);
+	sin.sin_family = AF_INET;
+	sin.sin_addr.s_addr = 0;
+	sin.sin_port = htons(SRV_PORT);
+	bind(s, (sockaddr*)&sin, sizeof(sin));
+	string msg, msg1;
+	double imt;
+	listen(s, 3);
+	while (1) {
+		from_len = sizeof(from_sin);
+		s_new = accept(s, (sockaddr*)&from_sin, &from_len);
+		cout << "new connected client! " << endl;
+		while (1) {
+			from_len = recv(s_new, (char*)&B, sizeof(B), 0);//читаем
+			buf[from_len] = 0;
+			if (B.height == -1) {
+				break;
+			}
+			imt = IMG(B.height, B.weight);
+			msg = to_string(imt);
+			B.IMT = msg;
+			//B.text = F(imt);
+			send(s_new, (char*)&B, sizeof(B), 0); // отправил
+		}
+		cout << "client is lost";
+		//closesocket(s_new);
+	}
+	return 0;
+}
